0fa05d3d71dc4f8b4fe1b182454e80402f54691c
NikitaaSharma <nikitasharma211999@gmail.com>
2024-04-24 17:13:52 +0530

    Boiler plate

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
             NavigationView{
                 ScrollView{
                     VStack(alignment: .leading, spacing: 24) {
-                        Text("Checking git push again")
+                        Text("Expense Tracker")
                             .font(.title2)
                             .bold()
                     }
