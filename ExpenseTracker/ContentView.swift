9803cb559125199532ab836bb11dcb64007ec65d
NikitaaSharma <nikitasharma211999@gmail.com>
2024-04-25 14:51:12 +0530

    Added TransactionList, RecentTransactionList

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
                         Text("Expense Tracker")
                             .font(.title2)
                             .bold()
+                        
+                        // MARK: Transaction List
+                        RecentTransactionList()
                     }
                     .padding()
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
@@ -35,15 +38,18 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transactions = transactionListPreviewData
+        return transactionListVM
+    }()
+
     static var previews: some View {
         Group{
             ContentView()
             ContentView()
                 .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
         }
+        .environmentObject(transactionListVM)
     }
-}
-
-#Preview {
-    ContentView()
 }
---
+++ ExpenseTracker/RecentTransactionList.swift
@@ -0,0 +1,63 @@
+//
+//  RecentTransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Nikita Sharma on 24/04/24.
+//
+
+import SwiftUI
+
+struct RecentTransactionList: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
+    var body: some View {
+        VStack {
+            HStack {
+                // MARK: Header Title
+                Text("Recent Transactions")
+                    .bold()
+                
+                Spacer()
+                
+                // MARK: Header Link
+                NavigationLink {
+                    TransactionList()
+                } label: {
+                    HStack(spacing: 4) {
+                        Text("See All")
+                        Image(systemName: "chevron.right")
+                    }
+                    .foregroundColor(Color.text)
+                }
+            }
+            .padding(.top)
+            
+            // MARK: Recent Transaction List
+            ForEach(Array(transactionListVM.transactions.prefix(5).enumerated()),id: \.element) { index, transaction in
+                TransactionRow(transaction: transaction)}
+            
+            Divider()
+//                .opacity(index == 4 ? 0 : 1)
+        }
+        .padding()
+        .background(Color.systemBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color.primary.opacity(0.2), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x:0, y:5)
+    }
+}
+
+struct RecentTransactionList_Preview: PreviewProvider {
+    static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transactions = transactionListPreviewData
+        return transactionListVM
+    }()
+    
+    static var previews: some View {
+        Group {
+            RecentTransactionList()
+            RecentTransactionList()
+                .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        }.environmentObject(transactionListVM)
+    }
+}
